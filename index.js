633f1b176b7682f3d171a619215b03c061fac0df
Tray Lewin <traylewin@Trays-iMac.local>
2020-01-07 11:02:42 -0800

    change to more standard eslint add typescript support

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
 module.exports = {
-  "extends": ["airbnb", "eslint:recommended"],
+  "extends": [
+    "airbnb-typescript/base",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "plugins": [
+    "@typescript-eslint",
+    "prettier",
+    "jest",
+    "react", 
+    "react-hooks"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2018, // Allows for the parsing of modern ECMAScript features
+    "sourceType": "module" // Allows for the use of imports
+  },
   "env": {
     "browser": true,
     "node": true,
     "jest": true
   },
-  "parser": "babel-eslint",
+  "overrides": [
+    {
+      "files": ["*.test.*"],
+      "rules": {
+        "no-undefined": "off"
+      }
+    }
+  ],
   "rules": {
-    "indent": ["error", 2],
-    "import/prefer-default-export": 0,
-    "prefer-destructuring": 1,
-    "no-await-in-loop": 0,
-    "consistent-return": 1,
-    "arrow-parens": [2, "always"],
+    "no-underscore-dangle": "off",
+    "prettier/prettier": "error",
+    "object-curly-newline": "off",
+    // "no-unused-vars": "off",
+    "no-continue": "off",
+    "max-len": [
+      2,
+      {
+        "code": 600
+      }
+    ],
+    "comma-dangle": ["error", "always-multiline"],
+    // "no-param-reassign": 0,
+    "import/prefer-default-export": "off",
+    // "prefer-destructuring": 0,
+    "camelcase": 0,
+    // "array-callback-return": 0,
+    "quotes": ["error", "single"],
+    "semi": "off",
+    "@typescript-eslint/semi": [2, "never"],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
-    "semi": [2, "always"],
-    "no-console": 0,
-    "class-methods-use-this": 0,
-    "generator-star-spacing": 0,
-    "object-curly-newline": 0,
-    "operator-linebreak": 0,
-    "max-len": [
-      "error",
-      120
-    ],
+    "react/destructuring-assignment": 0,
     "react/jsx-filename-extension": 0,
     "react/prop-types": 0,
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "implicit-arrow-linebreak": ["error", "beside"],
-    "nonblock-statement-body-position": ["error", "beside"],
-    "brace-style": [2, "1tbs", { "allowSingleLine": true }],
-    "no-multi-spaces": "error",
-    "jsx-quotes": [
-      "error",
-      "prefer-single"
-    ],
-    "react/destructuring-assignment": 0,
+    "jsx-quotes": [ "error", "prefer-single" ],
   },
-  "plugins": ["react", "react-hooks"],
   "globals": {
     "Web3": false
   },
